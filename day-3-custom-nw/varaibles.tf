8073962d6f816cfab8042103bad6a789f666b3bf
sureshkethavathrockstar <kethavathsuresh357@gmail.com>
2025-01-04 17:09:16 +0530

    Update varaibles.tf

--- day-3-custom-nw/varaibles.tf
+++ day-3-custom-nw/varaibles.tf
@@ -1,15 +1,15 @@
 variable "ami_id" {
     description = "passing AMI vlaue to  main.tf"
     type = string
-    default = "ami-0fd05997b4dff7aac"
+    default = ""
   
 }
 variable "instance_type" {
   type = string
-  default = "t2.micro"
+  default = ""
 }
 variable "key_name" {
     type = string
-    default = "Ram"
+    default = ""
 #test
 }
